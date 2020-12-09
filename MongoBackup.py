846fa3fa71e52cf942c105aebc7cedc21eefe29d
walaaelgenidy <45464802+walaaelgenidy@users.noreply.github.com>
2020-12-09 16:35:49 +0200

    Update MongoBackup.py

--- MongoBackup.py
+++ MongoBackup.py
@@ -8,15 +8,15 @@ import gzip
 
 #database info and backup file
 BACKUP_DIR_NAME ='/mongo_backups'
-DBHOST = '3.19.232.40'
-PORT = '8080'
-DBUSER = 'mongoAdmin'
-DBUSERPASSWORD = 'changeMe'
+DBHOST = '127.0.0.1'
+PORT = '27017'
+DBUSER = 'root'
+DBUSERPASSWORD = '123456'
 DBNAME = 'admin'
 
 # Getting current DateTime
-DATETIME = time.strftime('%Y%m%d-%H%M%S')
-TODAYBACKUPPATH = BACKUP_DIR_NAME + '/'+ DATETIME
+DATETIME = time.strftime('%Y%m%d-%H%M')
+TODAYBACKUPPATH = BACKUP_DIR_NAME + "/" +DATETIME
 
 # check if the backup folder exist or not
 try:
@@ -26,9 +26,9 @@ except:
 
 #starting backups
 db = DBNAME
-dumpcmd = "mongoexport --host=" + DBHOST + "--port=" + PORT + "--username=" + DBUSER + "--password=" + DBUSERPASSWORD + "--pretty" +  "--db=" + db + "--out=" + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".json"
+dumpcmd = "mongodump --host=" + DBHOST + " --port=" + PORT + " --username="+ DBUSER + " --password=" + DBUSERPASSWORD +  " --db=" + db + " --out=" + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".bson"
 os.system(dumpcmd)
-gzipcmd = "gzip" + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".json"
-os.system(gzipcmd)
+#gzipcmd = "gzip " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".json"
+#os.system(gzipcmd)
 
 print("Backup completed and have been created in" + TODAYBACKUPPATH + "directory")
